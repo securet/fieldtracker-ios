9b1b4f8ff5f5e319892a48586eb63fc5d1d00f9a
User1 <user1@rohits-macbook-pro.local>
2016-12-27 09:20:16 +0530

    Commit

---
+++ OppoAttendanceTracker/MKGetLocation.h
@@ -0,0 +1,13 @@
+//
+//  MKGetLocation.h
+//  Field Tracker
+//
+//  Created by User1 on 12/27/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKGetLocation : NSObject
+
+@end
---
+++ OppoAttendanceTracker/MKGetLocation.m
@@ -0,0 +1,13 @@
+//
+//  MKGetLocation.m
+//  Field Tracker
+//
+//  Created by User1 on 12/27/16.
+//
+//
+
+#import "MKGetLocation.h"
+
+@implementation MKGetLocation
+
+@end
